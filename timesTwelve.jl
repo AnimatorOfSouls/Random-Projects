83bc8e30e3a710c5e36c8b129d66d9934ac76f93
AnimatorOfSouls <33594615+AnimatorOfSouls@users.noreply.github.com>
2019-12-13 12:07:19 +0000

    Update timesTwelve.jl

--- timesTwelve.jl
+++ timesTwelve.jl
@@ -1,9 +1,20 @@
+module timesNum
+export timesFive,timesTwelve
+
+"""
+  timesFive(times_by::integer)
+
+Return 'times_by*5'
+"""
+
+timesFive(times_by::integer) = times_by*5
+
 """
   timesTwelve(times_by::integer)
 
 Return 'times_by*12'
 """
 
-timesTables(times_by::integer) = times_by*12
+timesTwelve(times_by::integer) = times_by*12
 
 end
